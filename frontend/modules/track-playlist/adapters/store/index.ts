2cef8d91ea281c1d89827cf654de75d583940eed
Armen Torosian <a.torosyan@wazzup.online>
2026-03-04 12:33:00 +0300

    fix(frontend): quickfix

--- frontend/modules/track-playlist/adapters/store/index.ts
+++ frontend/modules/track-playlist/adapters/store/index.ts
@@ -1,23 +1,15 @@
 import type { TPlaylist, TTrack } from "../../entities";
 
-export const useTrackPlaylistStore = defineStore(
-  "trackPlaylist",
-  () => {
-    const currentlyPlayingPlaylist = ref<TPlaylist>();
-    const currentlPlaylist = ref<TPlaylist>();
-    const currentlyPlayingTrack = ref<TTrack>();
-    const isPlaying = ref(false);
+export const useTrackPlaylistStore = defineStore("trackPlaylist", () => {
+  const currentlyPlayingPlaylist = ref<TPlaylist>();
+  const currentlPlaylist = ref<TPlaylist>();
+  const currentlyPlayingTrack = ref<TTrack>();
+  const isPlaying = ref(false);
 
-    return {
-      currentlPlaylist,
-      currentlyPlayingPlaylist,
-      currentlyPlayingTrack,
-      isPlaying,
-    };
-  },
-  {
-    persist: {
-      storage: localStorage,
-    },
-  }
-);
+  return {
+    currentlPlaylist,
+    currentlyPlayingPlaylist,
+    currentlyPlayingTrack,
+    isPlaying,
+  };
+});
