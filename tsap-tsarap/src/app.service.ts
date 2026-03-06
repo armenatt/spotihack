ad270e3f98e18fd441cc7301b308d6c906ea9e82
Armen Torosian <a.torosyan@wazzup.online>
2026-03-06 04:02:01 +0300

    fix(tsap-tsarap): rate limit

--- tsap-tsarap/src/app.service.ts
+++ tsap-tsarap/src/app.service.ts
@@ -64,6 +64,7 @@ export class AppService implements OnModuleInit {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     const result = this.ytdlp.stream(videoId, {
       format: { filter: 'audioonly', quality: 10 },
+      limitRate: '10M',
     });
 
     await result.pipeAsync(writeStream);
