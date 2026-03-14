58138ac736497728ed108bbc4d3a8d44e6590ec4
Armen Torosian <a.torosyan@wazzup.online>
2026-03-14 21:59:27 +0300

    fix(frontend): 401

--- frontend/plugins/1.axios.ts
+++ frontend/plugins/1.axios.ts
@@ -10,7 +10,7 @@ async function onRejected(response: AxiosError) {
 
   const { accessToken } = storeToRefs(authStore);
   const { $services } = useNuxtApp();
-  if (response.status === 401 && localStorage.getItem("accessToken")) {
+  if (response.status === 401 && accessToken.value?.length) {
     const result = await $services.authService.refresh();
     localStorage.setItem("accessToken", result.data.accessToken);
     accessToken.value = result.data.accessToken;
